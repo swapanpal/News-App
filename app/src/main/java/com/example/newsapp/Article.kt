865030fb1123678f484a55303f895357cf3ea048
swapan <swapan.d83@gmail.com>
2022-03-27 17:14:48 +0600

    DAO and Article Setup Room

--- app/src/main/java/com/example/newsapp/Article.kt
+++ app/src/main/java/com/example/newsapp/Article.kt
@@ -1,9 +1,13 @@
 package com.example.newsapp
 
-/**
- * Auto generated class by json to kotlin class
- */
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "articles")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id : Int? = null,
     val author: String,
     val content: String,
     val description: String,
